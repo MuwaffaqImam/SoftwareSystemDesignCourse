ef70e3e0306c90fce8a51ac4bbe6284015ba30fc
Muwaffaq Imam <muwaffaqimam@gmail.com>
2021-10-08 13:31:43 +0300

    -- init project

---
+++ src/com/muwaffaq/innopolis/Main.java
@@ -0,0 +1,13 @@
+package com.muwaffaq.innopolis;
+
+public class Main {
+
+    /**
+     * This is the Main branch it does Nothing
+     * Every lab has it's own branch, so please check branches ..
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello!, please check branches for labs..");
+    }
+}
